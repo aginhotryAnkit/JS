4d3786a1cd5748a15c94874c7297c9011b212de2
ankit aginhotry <akaginhotry9651@gmail.com>
2023-12-02 14:14:38 +0530

    undefined vs not defined vs null Comparsion

---
+++ undefined.js
@@ -0,0 +1,25 @@
+var a=10;
+if(a===undefined){
+  console.log("a is undefined");
+} else {
+  console.log("a is not undefined have some value: ",a);
+}
+
+// 'undefined' :- undefined live a place holder its hold the place of the variable 
+//              untill the variable is not aissgin a value of itself;
+
+// 'not defiend' :-  not defined is error that is endicate that variable try to access is 
+//                 is not defined yet and not in a Heap Memory. or Laxical Space;
+
+// error look like that [ReferenceError: x is not defined]
+
+// 'null' :- dont confused about the null because null is a value which is assign to a variable
+//         when the user want to store nothing in variable, It is often used to initialize a
+//         variable to an "empty" or "unknown" state;
+
+
+//Note :- Null is endicate the absence or empty state of the var and undefiend denote that the value
+//variable not has been intilize or declare or asssign yet.
+
+
+// .....Poco Loco 
