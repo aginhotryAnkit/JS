258edcf7095d7f35956d12d2921f29a9cd46409f
ankit aginhotry <akaginhotry9651@gmail.com>
2023-12-13 23:38:52 +0530

    more information is updated about the arrow function in typeofFunction fie

--- typeOfFunctions.js
+++ typeOfFunctions.js
@@ -4,8 +4,9 @@ function hello(){
 }
 
 //Function expression 
-var hii = function (){
+var hii = function (callback){
     console.log("Hii")
+    console.log(callback());
 };
 
 //Annonymous function
@@ -32,6 +33,26 @@ function x(parm1, parm2){ // when the function is declare and need to take some 
     x(arg1,arg2) // as you can see when a value pass when the function are called as argument 
 }
 
-//arrow function
-
+//Arrow function
 // arrow function are compact alternate to function expression its traditional function introduce is ECMA Script 2019
+// In JavaScript, an arrow function is a shorter way to write a function. It's like a shorthand for creating functions. Here's a simple breakdown:
+
+// Basic Syntax:
+
+// Instead of writing a function like this:
+function add(a, b) {
+  return a + b;
+}
+
+// You can use an arrow function:
+const add = (a, b) => a + b; //arrow function 
+
+
+// No Need for the function Keyword:
+// With arrow functions, you don't need to use the function keyword. You just write the parameters, an arrow (=>), and then the expression you want to return.
+
+// Single Expression Shortcuts:
+// If your function has only one expression, like the a + b in the example, you can omit the curly braces {} and the return keyword. The result is implicitly returned.
+
+// Useful for Short Functions:
+// Arrow functions are often used for shorter functions, especially when you need a quick, simple way to define them.
