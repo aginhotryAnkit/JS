c8035d1339f447e61e1afb13f8d8c13692ff0c29
ankit aginhotry <akaginhotry9651@gmail.com>
2023-12-31 14:01:33 +0530

    spelling mistaker correct now

--- callBackIssue.js
+++ callBackIssue.js
@@ -23,18 +23,18 @@ console.log("Code Execution Ended..");
 
 /**
  * Now there is a trust issue with the settime out function in the given example
- * we have decalre a callback function which is need to run after the 2 seconds
- * but function did't run after the 2 second becase after the settimeout function
- * there is while loop witch is run by 5 second and when program is run a callfunction
- * is registed in the web api and after the that rest of the code means while loop
- * is running by 5 second after but in that time settimeout function need to be executeed
- * but did't executed. The reson behild not executed is that there is an callback queue 
- * and event loop in the js enviroment event loop is keep checking if the callsack is empty
+ * we have declare a callback function which is need to run after the 2 seconds
+ * but function did't run after the 2 second because after the settimeout function
+ * there is while loop witch is run by 5 second and when program is run a call function
+ * is register in the web api and after the that rest of the code means while loop
+ * is running by 5 second after but in that time settimeout function need to be executed
+ * but did't executed. The reason behind not executed is that there is an callback queue 
+ * and event loop in the js environment event loop is keep checking if the callstack is empty
  * then it will get the callback function from the callback queue and and put inside the
  * callstack and callstack is executed it. here because callstack is still executed the
- * GEC [global execulation contaxt]. and when the whole code inside the GEC is run
+ * GEC [global execution context]. and when the whole code inside the GEC is run
  * then GEC pop off the callstack and and at that time event loop check callstack is empty
- * it will quickly pick the callback from the the callback queueu and put the callback function
+ * it will quickly pick the callback from the the callback queue and put the callback function
  * inside the callstack and callstack executed callback function.
  * 
  * that how the whole program is being executed.
