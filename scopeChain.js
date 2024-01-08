671021fc0481a50a2600f11803de973f218cab0a
ankit aginhotry <akaginhotry9651@gmail.com>
2024-01-08 11:30:52 +0530

    scope chain updated

--- scopeChain.js
+++ scopeChain.js
@@ -1,12 +1,20 @@
-function a(){
-    var xx = 20;
-    b();
-    function b(){
-        console.log(xx);
-    }
-
+function one(){
+    console.log(a); //this a is refer to the global scope thats why printed the 50. because this function is present in the global scope
 }
 
 
-var x = 10;
-a();
+function two(){
+    var a = 10;
+
+    function three(){
+        console.log(a); //this is inside a function so that first search in local scope then parent so that is printed as 10
+    }
+    one();
+    three();
+}
+
+
+var a = 50;
+two();
+
+//output 50, 10
